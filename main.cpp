e8c73ab999eceabc874de2c28cacf3e2944c059a
Gleb Abroskin <abroskingleb@gmail.com>
2016-06-23 23:35:46 +0300

    add comments, found stupid error in creating avg and excel files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "main.h"
 
 int main(){
+// Random engine
 	std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dist(1,40);
@@ -9,13 +10,16 @@ int main(){
 	std::vector<float> timeDP;
 	std::vector<float> timeBF;
 
+// Storing average time
 	std::vector<float> avgTimeDP(MAX_SIZE);
 	std::vector<float> avgTimeBF(MAX_SIZE);
 	
 	for (int attempt = 0; attempt < ATTEMPS; attempt++){
+		/* To fix strange error with sizes of vectors */
 		timeDP.resize(MAX_SIZE);
 		timeBF.resize(MAX_SIZE);
 
+		/* Create new files for each attempt */
 		std::string fileName;
 		fileName = "results/";
     	fileName += "attempt#" + std::to_string(attempt+1)  + ".txt";
@@ -32,30 +36,32 @@ int main(){
     		for (int i=0; i < size; i++)
     			t[i] = dist(gen);
  
-
+    		/* Counting time to dynamic programming */
 			time_t DP_start = clock();
 			dynamicSolution(t);
 			time_t DP_end = clock();
 
-			timeDP.at(size-2) = float(DP_end - DP_start); 	//количество тактов
-			timeDP.at(size-2) /= CLOCKS_PER_SEC;			//количество секунд
-			timeDP.at(size-2) *= 1000;						//перевод в мc
+			timeDP.at(size-2) = float(DP_end - DP_start); 	// количество тактов
+			timeDP.at(size-2) /= CLOCKS_PER_SEC;			// количество секунд
+			timeDP.at(size-2) *= 1000;						// перевод в мc
 
+			/* Counting time to brute force */
 			time_t BF_start = clock();
 			bruteForce(t);
 			time_t BF_end = clock();
 
-			timeBF.at(size-2) = float(BF_end - BF_start); 	//количество тактов
-			timeBF.at(size-2) /= CLOCKS_PER_SEC;			//количество секунд
-			timeBF.at(size-2) *= 1000;						//перевод в мc
+			timeBF.at(size-2) = float(BF_end - BF_start); 	// количество тактов
+			timeBF.at(size-2) /= CLOCKS_PER_SEC;			// количество секунд
+			timeBF.at(size-2) *= 1000;						// перевод в мc
 
+			/* fill vectors with time */
 			avgTimeDP.at(size-2) += timeDP.at(size-2);
 			avgTimeBF.at(size-2) += timeBF.at(size-2);
 
 			cout.width(6);
 			out << size << "\t\t";
 			cout.width(10);
-			out << timeDP.at(size-2)<< "\t\t";
+			out << timeDP.at(size-2) << "\t\t";
 			cout.width(10);
 			out  << timeBF.at(size-2) << "\n";
 
